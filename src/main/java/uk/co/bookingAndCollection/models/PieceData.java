a24bc7e98fb1eef8c0c1afd2f7023c40840628b8
Isambard Wheeler-artwell <isambardwheeler-artwell@Isambards-Air.home>
2018-09-06 17:21:08 +0100

    Added models PieceData and booking

--- src/main/java/uk/co/bookingAndCollection/models/PieceData.java
+++ src/main/java/uk/co/bookingAndCollection/models/PieceData.java
@@ -1,5 +1,47 @@
 package uk.co.bookingAndCollection.models;
 
 public class PieceData {
+	
+	private Integer id;
+	
+	private String length;
+	
+	private String width;
+	
+	private String height;
+	
+	private String weight;
+	
+	public String getWeight() {
+		return weight;
+	}
 
+	public void setWeight(String weight) {
+		this.weight = weight;
+	}
+
+	public String getLength() {
+		return length;
+	}
+
+	public void setLength(String length) {
+		this.length = length;
+	}
+
+	public String getWidth() {
+		return width;
+	}
+
+	public void setWidth(String width) {
+		this.width = width;
+	}
+
+	public String getHeight() {
+		return height;
+	}
+
+	public void setHeight(String height) {
+		this.height = height;
+	}
+	
 }
--- src/main/java/uk/co/bookingAndCollection/models/booking.java
+++ src/main/java/uk/co/bookingAndCollection/models/booking.java
@@ -1,15 +1,14 @@
 package uk.co.bookingAndCollection.models;
 
 import java.util.List;
-
 import javax.persistence.*;
 
 @Entity
-public class booking {
+public class Booking {
 
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
-	private Long Id;
+	private Long id;
 	
 	private String customerName;
 	
@@ -19,7 +18,7 @@ public class booking {
 	
 	private String postCode;
 	
-	private String City;
+	private String city;
 	
 	private String specialInstructions;
 	
@@ -34,6 +33,104 @@ public class booking {
 	private String bookingTime;
 	
 	private List<PieceData> piecesDatas;
+	
+	public String getCustomerName() {
+		return customerName;
+	}
+
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+
+	public String getHouseNumber() {
+		return houseNumber;
+	}
+
+	public void setHouseNumber(String houseNumber) {
+		this.houseNumber = houseNumber;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getPostCode() {
+		return postCode;
+	}
+
+	public void setPostCode(String postCode) {
+		this.postCode = postCode;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getSpecialInstructions() {
+		return specialInstructions;
+	}
+
+	public void setSpecialInstructions(String specialInstructions) {
+		this.specialInstructions = specialInstructions;
+	}
+
+	public String getGeneralDescription() {
+		return generalDescription;
+	}
+
+	public void setGeneralDescription(String generalDescription) {
+		this.generalDescription = generalDescription;
+	}
+
+	public String getIsHazardousGoods() {
+		return isHazardousGoods;
+	}
+
+	public void setIsHazardousGoods(String isHazardousGoods) {
+		this.isHazardousGoods = isHazardousGoods;
+	}
+
+	public String getIsLooselyPacked() {
+		return isLooselyPacked;
+	}
+
+	public void setIsLooselyPacked(String isLooselyPacked) {
+		this.isLooselyPacked = isLooselyPacked;
+	}
+
+	public String getBookingDate() {
+		return bookingDate;
+	}
+
+	public void setBookingDate(String bookingDate) {
+		this.bookingDate = bookingDate;
+	}
+
+	public String getBookingTime() {
+		return bookingTime;
+	}
+
+	public void setBookingTime(String bookingTime) {
+		this.bookingTime = bookingTime;
+	}
+
+	public List<PieceData> getPiecesDatas() {
+		return piecesDatas;
+	}
+
+	public void setPiecesDatas(List<PieceData> piecesDatas) {
+		this.piecesDatas = piecesDatas;
+	}
+
+
 	
 	
 }

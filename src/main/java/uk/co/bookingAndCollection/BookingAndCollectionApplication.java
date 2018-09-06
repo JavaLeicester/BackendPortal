f895f041390c299af81894e4dd99f82a2dddb076
Isambard Wheeler-artwell <isambardwheeler-artwell@Isambards-Air.home>
2018-09-06 14:38:49 +0100

    Initial commit

---
+++ src/main/java/uk/co/bookingAndCollection/BookingAndCollectionApplication.java
@@ -0,0 +1,12 @@
+package uk.co.bookingAndCollection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingAndCollectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingAndCollectionApplication.class, args);
+	}
+}
---
+++ src/test/java/uk/co/bookingAndCollection/BookingAndCollectionApplicationTests.java
@@ -0,0 +1,16 @@
+package uk.co.bookingAndCollection;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BookingAndCollectionApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

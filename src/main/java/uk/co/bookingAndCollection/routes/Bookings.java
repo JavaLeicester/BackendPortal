b2db0b2be1582d108cc5f73cb9ca522ec48d8efa
Isambard Wheeler-artwell <isambardwheeler-artwell@host-160-154.eduroam-local.wifi.le.ac.uk>
2018-09-06 18:35:05 +0100

    Created routes directory

---
+++ src/main/java/uk/co/bookingAndCollection/routes/Bookings.java
@@ -0,0 +1,14 @@
+package uk.co.bookingAndCollection.routes;
+
+import org.springframework.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping(path="/api/bookings")
+public class Bookings {
+	
+	
+
+}

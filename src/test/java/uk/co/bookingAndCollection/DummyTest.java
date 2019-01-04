80a8800d3526e4492b28e68e36fba0eeba7db6d3
Isambard Wheeler-Artwell <wheelerisambard@hotmail.co.uk>
2019-01-04 13:18:00 +0000

    Check to see whether test also fails on circleci

---
+++ src/test/java/uk/co/bookingAndCollection/DummyTest.java
@@ -0,0 +1,19 @@
+package uk.co.bookingAndCollection;
+
+import org.hamcrest.*;
+import org.junit.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class DummyTest {
+
+	
+	@Test
+	public void isStringEmpty() {
+		
+		String stringToTest="ad";
+		
+		assertThat(stringToTest, Matchers.isEmptyString());
+		
+	}
+	
+}
